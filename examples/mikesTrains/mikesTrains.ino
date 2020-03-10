8f5d7363816fc5d26bb898d16cbbece8cbe4c3ba
Ryan Mack <rmack@datto.com>
2020-03-10 12:32:58 -0400

    Add support for randomly changing one light at a time

--- examples/mikesTrains/mikesTrains.ino
+++ examples/mikesTrains/mikesTrains.ino
@@ -1,4 +1,4 @@
-/*************************************************** 
+ /*************************************************** 
   This is an example for our Adafruit 24-channel PWM/LED driver
 
   Pick one up today in the adafruit shop!
@@ -39,32 +39,50 @@ void setup() {
 }
 
 void loop() {
-    randomChange(); 
-    delay(10000);
+    // This function is repeatedly executed
+    // Change the function that goes in here to change functionality of the lights
+    randomChangeOneLight();
+    delay(5000); // Change this number to the wait in milliseconds between each light change
 }
 
-
-// Fill the dots one after the other with a color
+// Call this function in loop() to turn all the lights on
 void lightsOn() {
   for(uint16_t i=0; i<24*NUM_TLC5974; i++) {
-      tlc.setPWM(i, 4095);
-      tlc.write();
+      tlc.setPWM(i, 4095); // Change the second number to change brightness of leds on scale from 0 to 4095
   }
+  tlc.write();
 }
 
+// Call this function in loop() to turn all the lights off
 void lightsOff() {
   for(uint16_t i=0; i<24*NUM_TLC5974; i++) {
       tlc.setPWM(i, 0);
-      tlc.write();
   }
+  tlc.write();
 }
 
+// Call this function in loop() to randomly turn 1 of your lights on or off.
+void randomChangeOneLight() {
+  int lightIndex = rand() % (24*NUM_TLC5974 - 1);
+  bool randBool = rand() & 1;
+
+  if (randBool) {
+    tlc.setPWM(lightIndex, 4095); // Change the second number to change brightness of leds on scale from 0 to 4095
+  }
+  else {
+    tlc.setPWM(lightIndex, 0);
+  }
+
+  tlc.write();
+}
+
+// Call this function in loop() to randomly change all the lights at once
 void randomChange() {
   bool randbool;
   for(uint16_t i=0; i<24*NUM_TLC5974; i++) {
       randbool = rand() & 1;
       if (randbool) {
-        tlc.setPWM(i, 4095);
+        tlc.setPWM(i, 4095); // Change the second number to change brightness of leds on scale from 0 to 4095
       }
       else {
         tlc.setPWM(i, 0);
